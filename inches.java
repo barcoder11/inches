bc486299fe41427e9d0ea226d4de19449ffabb8f
barcoder11 <159988763+barcoder11@users.noreply.github.com>
2025-09-22 19:37:09 -0400

    Add files via upload

---
+++ inches.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+/**
+ * User can type the number of inches and the progam will output the feet. 
+ * @author Sangay Pemba
+ * @since 09/20/25
+ */
+
+public class InchesToFeet {
+    public static void main(String[] args ) {
+        theInches(); // call the method that does the conversion
+    }
+
+     public static void theInches() {
+       
+        // create a scanner to store value of the user
+        Scanner scanner = new Scanner(System.in);
+       
+        //Ask User for the nunber of inches
+     System.out.println("Enter the number of inches:");
+     int inches = scanner.nextInt();
+     
+    //Calculates
+     int feet = inches / 12; // feet calculation
+     int reMainder = inches % 12; // leftover
+    
+     System.out.println(inches + " inches is " + feet + " feet, and " + reMainder + " inches.");
+   
+     scanner.close();
+
+    }
+
+    
+}
+/*
+ -Reflection:
+ - I learned how to use the Scanner class for inputs
+ - I learned % gives the remainder inches.
+ */
